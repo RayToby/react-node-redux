04dde00fca58fbf4433c565c2d607da99103f214
RayToby <623501500@qq.com>
2018-07-20 18:17:11 +0800

    '爬取数据写入数据库'

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const superagent = require('superagent');
+const async = require('async'); 
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost:27017/users');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const mysql = require('mysql');
 
-//const db = mongoose.connection;
+const conn = mysql.createConnection({
+    host: '192.168.1.114',//数据库地址
+    user: 'root',//账号
+    password: '123456',//密码
+    database: 'lagou',//库名
+    multipleStatements: true //允许执行多条语句
+});
 
-var _data = [{
+let _data = [{
     "id":"1",
     "name":"zysoft",
     "sex":"男",
@@ -31,6 +41,71 @@ var _data = [{
     "sex":"女",
     "age":"23"
 }];
+
+let urlArray = [];
+let url = '';
+let jsonData = [];
+for(let i = 1; i <= 30; i++) {
+    if(i === 1) {
+        url = 'https://www.lagou.com/zhaopin/?filterOption=3';
+    }else {
+        url = 'https://www.lagou.com/zhaopin/'+i+'/?filterOption=3';
+    }
+    urlArray.push(url);
+}
+
+setInterval(getDataToDataBase,300000);   //5分钟拉取一次数据
+function getDataToDataBase(){
+// app.get('/',(req, res) => {
+    async.mapLimit(urlArray, 5, (baseUrl, callback) => {
+        superagent.get(baseUrl).end((err, result) => {
+            if(err) console.errpr(err);
+            let $ = cheerio.load(result.text);
+
+            $('.item_con_list .con_list_item').each((index, element) => {
+                let $element = $(element);
+                let $position = $element.find('.position');
+                let $company = $element.find('.company');
+
+                let position = $position.find('.position_link').children('h3').text().trim();
+                let address = $position.find('.position_link').children('.add').children('em').text().trim();
+                let salary = $position.find('.p_bot').find('.money').text().trim();
+                let requirement = $position.find('.p_bot').find('.li_b_l').text().trim();
+                let company_name = $company.find('.company_name').children('a').text().trim();
+                let industry = $company.find('.industry').text().trim();
+                let detail = $company.find('.company_name').children('a').attr('href');
+                let logo = $element.find('.com_logo').children('a').children().attr('src');
+
+                let selectSQL = `INSERT INTO lagou (position, address, salary, requirement, company_name, industry, detail, logo) VALUE('${position}', '${address}','${salary}','${requirement}','${company_name}','${industry}','${detail}','${logo}')`;
+                conn.query(selectSQL, function(err, rows) {
+                    if (err) throw err;
+                    else console.log('成功')
+                });
+                // jsonData.push({
+                //     position: $position.find('.position_link').children('h3').text().trim(),
+                //     adress: $position.find('.position_link').children('.add').children('em').text().trim(),
+                //     salary: $position.find('.p_bot').find('.money').text().trim(),
+                //     requirement: $position.find('.p_bot').find('.li_b_l').text().trim(),
+                //     company_name: $company.find('.company_name').children('a').text().trim(),
+                //     industry: $company.find('.industry').text().trim(),
+                //     detail: $company.find('.company_name').children('a').attr('href'),
+                //     logo: $element.find('.com_logo').children('a').children().attr('src'),
+                // });
+            });
+            // callback(null,JSON.stringify(jsonData));
+        });
+    },(err, result) => {
+        // res.send(result);
+        // fs.writeFile('laGou.json', result, (err) => {
+        //     if (err) console.error(err);
+        //     console.log("数据写入成功！");
+        // });
+    })
+// })
+
+}
+
+
 //设置跨域请求
 // app.all('*', function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -55,3 +130,4 @@ app.listen(8888,function(){
 // app.use(express.static("src")).listen(8888, (req, res) => {
 //     console.log('port at 8888')
 // });
+
--- src/components/Content.js
+++ src/components/Content.js
@@ -8,14 +8,34 @@ import 'antd/dist/antd.css';
 
 export default class Content extends PureComponent {
     state = {
-        data: [],
+        dataList: [],
         name: '',
         sex: '',
         age: '',
     }
 
     componentDidMount() {
-        fetch('/introduce')
+        // fetch('/introduce')
+        // .then((response) => {
+        //     if (response.status >= 400) {
+        //         throw new Error("Bad response from server");
+        //     }
+        //     return response.json();
+        // })
+        // .then((res) => {
+        //     this.setState({
+        //         data: res,
+        //         name: res.name,
+        //         sex: res.sex,
+        //         age: res.age
+        //     });
+        // });
+
+        // $.get('http://localhost:8888/introduce',function(res) {
+        //     console.log(res)
+        // })
+
+        fetch('/laGou')
         .then((response) => {
             if (response.status >= 400) {
                 throw new Error("Bad response from server");
@@ -23,37 +43,65 @@ export default class Content extends PureComponent {
             return response.json();
         })
         .then((res) => {
+            let data = eval('(' + res + ')');
             this.setState({
-                data: res,
-                name: res.name,
-                sex: res.sex,
-                age: res.age
+                dataList: data,
             });
         });
-        // $.get('http://localhost:8888/introduce',function(res) {
-        //     console.log(res)
-        // })
     }
 
     render() {
-        const { data } = this.state;
+        const { dataList } = this.state;
         const columns = [{
-            title: '姓名',
-            dataIndex: 'name',
-            key: 'name',
-          }, {
-            title: '年龄',
-            dataIndex: 'age',
-            key: 'age',
-          }, {
-            title: '性别',
-            dataIndex: 'sex',
-            key: 'sex',
+            title: '企业logo',
+            dataIndex: 'logo',
+            key: 'logo',
+            render: (value, row, index) => {
+                return(
+                    <img key={index} src={value} width="50"/>
+                )
+            }
+        }, {
+            title: '职位名称',
+            dataIndex: 'position',
+            key: 'position',
+        }, {
+            title: '公司名称',
+            dataIndex: 'company_name',
+            key: 'company_name',
+        }, {
+            title: '地点',
+            dataIndex: 'adress',
+            key: 'adress',
+        }, {
+            title: '行业 + 公司情况',
+            dataIndex: 'industry',
+            key: 'industry',
+        }, {
+            title: '要求',
+            dataIndex: 'requirement',
+            key: 'requirement',
+            render: (value, row, index) => {
+                return(
+                    <span key={index}>{value.substring(value.indexOf('经验'))}</span>
+                )
+            }
+        }, {
+            title: '薪资',
+            dataIndex: 'salary',
+            key: 'salary',
+        }, {
+            title: '公司详情',
+            dataIndex: 'detail',
+            key: 'detail',
+            render: (value, row, index) => {
+                return(<a href={value} key={index} target='_blank'>查看详情</a>)
+            }
         }]; 
         return(
             <div style={{paddingLeft: '20'}}>
                 <div>111</div>
-                <Table dataSource={data} columns={columns}/>
+                <Table dataSource={dataList} columns={columns}/>
             </div>
             
         )
